f35b7c2e38fc6438403436a9437507eda4e7c09a
Fitti <28876473+Fittiboy@users.noreply.github.com>
2019-12-15 17:16:54 +0100

    Convert settings setting to json and remove set_channel.py

--- set_channel.py
+++
@@ -1,8 +0,0 @@
-import shelve
-
-db = shelve.open('database')
-tokens = db['tokens']
-twitch = tokens['twitch']
-twitch['channel'] = input("New channel: ")
-tokens['twitch'] = twitch
-db['tokens'] = tokens
---
+++ settings.py
@@ -0,0 +1,11 @@
+import json
+
+with open('settings.json') as settings_file:
+    settings = json.load(settings_file)
+settings['username'] = input("Username: ")
+settings['client_id'] = input("Client-ID: ")
+settings['token'] = input("token: ")
+settings['channel'] = input("channel: ")
+settings['keepalive'] = int(input("Keepalive (in seconds): "))
+with open('settings.json', 'w') as settings_file:
+    json.dump(settings, settings_file)

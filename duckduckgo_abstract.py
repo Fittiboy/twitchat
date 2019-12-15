2574141381c902cecc3de75b5d0f885268a5da47
Fitti <28876473+Fittiboy@users.noreply.github.com>
2019-12-15 03:16:44 +0100

    Fix duckduckgo reference link representation

--- duckduckgo_abstract.py
+++ duckduckgo_abstract.py
@@ -1,6 +1,7 @@
 import requests
 
 def abstract(term):
+    term = "+".join(term.split(" "))
     url = f"https://api.duckduckgo.com/?q={term}&format=json&no_redirect=1&t=twitch_chatbot"
     r = requests.get(url).json()
     abstract = r['Abstract'][:300] + "[...]"
@@ -14,5 +15,4 @@ def abstract(term):
 
 if __name__ == "__main__":
     term = input("What is your search term? ")
-    term = "+".join(term.split(" "))
     print(abstract(term))

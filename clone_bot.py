1e5b4a2607c257701c80e5281b9b7b28121b2028
Fitti <28876473+Fittiboy@users.noreply.github.com>
2019-12-19 22:27:02 +0100

    Add way to clone and set up bot to new directory

---
+++ clone_bot.py
@@ -0,0 +1,33 @@
+from sys import argv
+from os import mkdir
+from shutil import copy
+import json
+import venv
+from subprocess import call
+
+with open("files_to_copy.json") as files_file:
+    filelist = json.load(files_file)
+
+path = argv[1]
+if path[-1] != "/":
+    path += "/"
+try:
+    mkdir(path)
+except FileExistsError:
+    pass
+
+shell_script = """#!/bin/sh
+cd {path}
+bash venv/bin/activate
+python3 -m pip install requests irc
+python3 settings.py""".format(path=path)
+
+for file in filelist:
+    copy(f"./{file}", path + file)
+
+with open(path + "setup.sh", "w") as setup_sh:
+    setup_sh.write(shell_script)
+
+venv.create(path + "/venv", system_site_packages=True)
+
+call(path + "setup.sh")

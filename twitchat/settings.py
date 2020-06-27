73be375a58ceebb3f96b8a5b545578ec35c30cfd
Fitti <dev@fitti.io>
2020-06-27 14:45:16 +0200

    Update settings.py

--- twitchat/settings.py
+++ twitchat/settings.py
@@ -1,14 +1,15 @@
 import json
 
-try:
-    with open('settings.json') as settings_file:
-        settings = json.load(settings_file)
-except FileNotFoundError:
-    pass
-settings['username'] = input("Username: ")
-settings['client_id'] = input("Client-ID: ")
-settings['token'] = input("token: ")
-settings['channel'] = input("channel: ")
-settings['keepalive'] = 300
-with open('settings.json', 'w') as settings_file:
-    json.dump(settings, settings_file, indent=4)
+if __name__ == "__main__":
+    try:
+        with open('settings.json') as settings_file:
+            settings = json.load(settings_file)
+    except FileNotFoundError:
+        pass
+    settings['username'] = input("Username: ")
+    settings['client_id'] = input("Client-ID: ")
+    settings['token'] = input("token: ")
+    settings['channel'] = input("channel: ")
+    settings['keepalive'] = 300
+    with open('settings.json', 'w') as settings_file:
+        json.dump(settings, settings_file, indent=4)

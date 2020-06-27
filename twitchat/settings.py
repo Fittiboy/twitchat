64818964a6a44e5488b7c35e12978cada1aac764
Fitti <dev@fitti.io>
2020-06-27 14:42:52 +0200

    Update settings to support packaging

--- twitchat/settings.py
+++ twitchat/settings.py
@@ -1,11 +1,14 @@
 import json
 
-with open('settings.json') as settings_file:
-    settings = json.load(settings_file)
+try:
+    with open('settings.json') as settings_file:
+        settings = json.load(settings_file)
+except FileNotFoundError:
+    pass
 settings['username'] = input("Username: ")
 settings['client_id'] = input("Client-ID: ")
 settings['token'] = input("token: ")
 settings['channel'] = input("channel: ")
-settings['keepalive'] = int(input("Keepalive (in seconds): "))
+settings['keepalive'] = 300
 with open('settings.json', 'w') as settings_file:
     json.dump(settings, settings_file, indent=4)

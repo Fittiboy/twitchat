ee130e8e2a75a6d48f79b41eb63b9e6c49b7b89a
Fitti <dev@fitti.io>
2021-09-07 19:43:25 +0200

    Handle Event variable assignment correctly

--- twitchat/commands.py
+++ twitchat/commands.py
@@ -56,7 +56,13 @@ class Commands:
         """Use this decorator to add a permission check to a command"""
         @functools.wraps(func)
         def permissions_wrapper(*args, **kwargs):
-            e = args[0]
+            e = args[1]
+            try:
+                e.tags
+            except AttributeError:
+                # Commands from extra_commands.py will
+                # have e as the 1st arg, instead of the 2nd
+                e = args[0]
             if len(args) == 4:
                 args = list(args)
                 args.insert(0, args[0].arguments[0])
